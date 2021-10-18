27ea6f6c434e1f16d60bf0bbfb3a8d53971082d1
Zan3Stoltz <z3stoltz@gmail.com>
2021-10-18 20:33:43 +0200

    Background

--- zipped/src/Post.js
+++ zipped/src/Post.js
@@ -2,21 +2,21 @@ import React from 'react';
 import './Post.css';
 import Avatar from "@material-ui/core/Avatar";
 
-function Post() {
+function Post({username, caption, imageUrl}) {
     return (
         <div className="post">
             <div classname = "post_header">
-            <Avatar
-                className="post_avatar"
-                alt='EmpoweredGirl'
-                src="/static/images/avatar/1.jpg"
-            />
-            <h3>Username</h3>
+                <Avatar
+                    className="post_avatar"
+                    alt='EmpoweredGirl'
+                    src="/static/images/avatar/1.jpg"
+                />
+                <h3>{username}</h3>
             </div>
 
-            <img className="post_image" src="https://projectwomanohio.files.wordpress.com/2014/03/empowerment.jpg" alt></img>
+            <img className="post_image" src= {imageUrl} alt=""/>
             
-            <h4 className="post_text"><strong>EmpoweredGirl: </strong> Feeling Empowered</h4>
+            <h4 className="post_text"><strong>{username}: </strong> {caption} </h4>
 
         </div>
     )
